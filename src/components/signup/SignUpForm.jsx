0dcb3fba8396ad5b94c5e889402f012ced3a05e1
yash-ghelani <yash.ghelani@sky.uk>
2023-01-17 13:22:18 +0000

    creating basic sign up form

--- src/components/signup/SignUpForm.jsx
+++ src/components/signup/SignUpForm.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
+import { useState } from "react";
 
 const SignUpForm = () => {
+  const defaultFormValues = {
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const [formValues, setFormValues] = useState(defaultFormValues);
+  const { displayName, email, password, confirmPassword } = formValues;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   return (
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required/>
+        <input
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={handleChange}
+          required
+        />
 
         <label>Email</label>
-        <input type="email" required/>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          required
+        />
 
         <label>Password</label>
-        <input type="password" required/>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          required
+        />
 
         <label>Confirm Password</label>
-        <input type="password" required/>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          required
+        />
 
-        <button type='submit'>Sign Up</button>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
-}
+};
 
-export default SignUpForm
+export default SignUpForm;
