a48e0703d90197d8a6daa453d9be565e3731bd10
yash-ghelani <yash.ghelani@sky.uk>
2023-02-10 13:20:16 +0000

    sign in/up forms styling changes

--- src/components/sign-in/signinform.style.jsx
+++ src/components/sign-in/signinform.style.jsx
@@ -4,9 +4,9 @@ export const SignInContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 500px;
-  box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.5);
   padding: 50px;
-  border-radius: 10px;
+  box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.5);
+  border-radius: 8px;
 
   h2 {
     margin: 10px 0;
--- src/components/sign-up/signupform.style.jsx
+++ src/components/sign-up/signupform.style.jsx
@@ -4,13 +4,11 @@ export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 500px;
-  border-radius: 8px;
   padding: 50px;
   box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.5);
-  border-radius: 10px;
+  border-radius: 8px;
 
   h2 {
     margin: 10px 0;
   }
 `;
-
