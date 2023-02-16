eda34c1967f21e79a94495d1d9288c8c25322bb9
yash-ghelani <yash.ghelani@sky.uk>
2023-02-16 17:13:38 +0000

    Redux migration complete - using redux devtools composer and hiding logger in production

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,38 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
 // middlewares are small libraries/logic
 // applied before actions hit the reducers
-const middlewares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middlewares))
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 const persistConfig = {
-  key: 'root', // where to start - root means start from the top
+  key: "root", // where to start - root means start from the top
   storage, // will use localStorage in the browser
-  blacklist: ['user'], // values we dont want to persist
-}
-
+  blacklist: ["user"], // values we dont want to persist
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // the store only needs a rootReducer
 // we add the extra params to visualise the store
-export const store = createStore(persistedReducer, undefined, composedEnhancers)
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
 export const persistor = persistStore(store);
