dfe8017989c51b2be87a2f059b293b1beb88bfd6
unknown <yashghelani08@gmail.com>
2023-03-01 18:38:03 +0000

    Updating favicon

--- src/components/categories/categories.style.jsx
+++ src/components/categories/categories.style.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const CategoriesContainer = styled.div`
   width: 95%;
-  margin: 0 auto;
+  margin: 40px auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
