0c33ab56c4eb7350dfbf8f9bf074b628b3e4e996
unknown <yashghelani08@gmail.com>
2023-03-01 20:10:13 +0000

    adding lazy/suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/nav/Navigation";
+import LoadingScreen from "./components/loading/Loading";
 import { useDispatch } from "react-redux";
 
-import { onAuthStateChangedListener, createUser } from "./utils/firebase/firebase";
+import {
+  onAuthStateChangedListener,
+  createUser,
+} from "./utils/firebase/firebase";
 import { setCurrentUser } from "./store/user/user-reducer";
 
-import Home from "./routes/home/Home";
-import Shop from "./routes/shop/Shop";
-import Accounts from "./routes/accounts/Accounts";
-import Checkout from "./routes/checkout/Checkout";
+import Navigation from "./routes/nav/Navigation";
+// import Home from "./routes/home/Home";
+// import Shop from "./routes/shop/Shop";
+// import Accounts from "./routes/accounts/Accounts";
+// import Checkout from "./routes/checkout/Checkout";
+
+const Home = lazy(() => import("./routes/home/Home"));
+const Shop = lazy(() => import("./routes/shop/Shop"));
+const Accounts = lazy(() => import("./routes/accounts/Accounts"));
+const Checkout = lazy(() => import("./routes/checkout/Checkout"));
 
 function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,18 +32,20 @@ function App() {
     });
 
     return unsubscribe;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="accounts" element={<Accounts />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<LoadingScreen/>}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="accounts" element={<Accounts />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
