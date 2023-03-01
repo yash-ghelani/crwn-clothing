d199b5f4677234b31edaf663f2ddd9d433f5e813
unknown <yashghelani08@gmail.com>
2023-03-01 17:39:26 +0000

    Final UI Change

--- src/components/category/category.style.jsx
+++ src/components/category/category.style.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
   max-width: 1000px;
+  width: 90%;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   column-gap: 20px;
   row-gap: 50px;
-  margin: auto;
+  margin: 0 auto 25px auto;
 `;
 
 export const CategoryTitle = styled.h2`
